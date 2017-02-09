0deba3b108694668467d32c8edc6f9a3e003a1bc
RanikaR <rmr29@njit.edu>
2017-02-08 20:35:07 -0500

    Added updated files

--- display_discount.php
+++ display_discount.php
@@ -1,3 +1,23 @@
+<?php
+	// gets the data from the form	
+	$desc       = $_POST["product_description"];
+	$list_price = $_POST["list_price"];
+	$disc_perc   = $_POST["discount_percentage"];
+
+	// calculates discount
+	$disc = $list_price * $disc_perc * .01;
+	$disc_price = $list_price - $disc;
+
+	// applies current formatting to dollar and percent amounts
+	$list_price_formatted = "$".number_format($list_price, 2);
+	$disc_perc_formatted = $disc_perc."%";
+	$disc_formatted = "$".number_format($disc, 2);
+	$disc_price_formatted = "$".number_format($disc_price, 2);
+
+	// escapes unformatted input
+	$product_descripion_escaped = htmlspecialchars($product_description);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,22 +26,22 @@
 </head>
 <body>
     <main>
-        <h1>This page is under construction</h1>
+        <h1>Product Discount Calculator</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product_description_escaped; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price_formatted; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $disc_perc_formatted; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $disc_formatted; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $disc_price_formatted; ?></span><br>
     </main>
 </body>
 </html>
